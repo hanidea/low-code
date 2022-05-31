1f6b2d3e5de070afd11a596a2883d7bf9659a40b
hanidea <hydavid@126.com>
2022-05-31 16:55:55 +0800

    no message

--- react/src/pages/function/hook/index.js
+++ react/src/pages/function/hook/index.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 export default function(props){
-  const [count, setCount] = useState()
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
+    console.log('useEffect')
+    fetch('/api/getLists')
+  },[count])
 
-  }, [])
+  const handleCount = ()=>{
+    setCount(count + 1)
+  }
 
   return (
     <div>
-      <h1>count:{count}</h1>
+      <h1 onClick={handleCount}>count:{count}</h1>
     </div>
   )
 }
