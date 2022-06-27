77afce1127be42f04d7b0800dd49e30480639d6f
hanidea <hydavid@126.com>
2022-06-27 10:02:38 +0800

    egg mysql5.7

---
+++ egg/config/plugin.js
@@ -0,0 +1,24 @@
+'use strict';
+const path = require('path')
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+};
+exports.ejs = {
+  enable: true,
+  package: 'egg-view-ejs'
+}
+exports.auth = {
+  enable: true,
+  path: path.join(__dirname, '../lib/plugin/egg-auth')
+}
+
+exports.mysql = {
+  enable:true,
+  package:'egg-mysql'
+}
+
+exports.sequelize = {
+  enable: true,
+  package:'egg-sequelize'
+};
