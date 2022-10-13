a709b9835aca8148c3f650662fe28ae1f96acc18
hanidea <hydavid@126.com>
2022-10-13 14:20:08 +0800

    prepare

--- 51hanhan-dev/core/cli/lib/index.js
+++ 51hanhan-dev/core/cli/lib/index.js
@@ -31,16 +31,14 @@ async function core() {
 }
 
 async function checkGlobalUpdate(){
-    //1.获取当前的版本号和模版名
     const currentVersion = pkg.version;
     const npmName = pkg.name;
-    //2.调用npm API, 获取所有的版本号
-    const {getNpmInfo} = require('@51hanhan-dev/get-npm-info');
-    const data = await getNpmInfo(npmName);
-    console.log(data);
-
-    //3.提取所有版本号，比对哪些版本号是大于当前版本号
-    //4.获取最新的版本号，提示用户更新到该版本
+    const { getNpmSemverVersion } = require('@51hanhan-dev/get-npm-info');
+    const lastVersion = await  getNpmSemverVersion(currentVersion,npmName);
+    if(lastVersion && semver.gte(lastVersion,currentVersion)){
+        log.warn(colors.yellow(`请手动更新${npmName},当前版本:${currentVersion},最新版本:${lastVersion} 
+        更新命令: npm install -g ${npmName}`));
+    };
 
 }
 
--- 51hanhan-dev/utils/get-npm-info/lib/index.js
+++ 51hanhan-dev/utils/get-npm-info/lib/index.js
@@ -21,10 +21,38 @@ function getNpmInfo(npmName, registry) {
     })
 }
 
-function getDefaultRegistry(isOriginal = false){
+function getDefaultRegistry(isOriginal = true){
+    console.log(isOriginal);
     return isOriginal ? 'https://registry.npmjs.org':'https://registry.npm.taobao.org'
+}
+
+async function getNpmVersions(npmName, registry){
+    const data = await getNpmInfo(npmName,registry);
+    if(data){
+        return Object.keys(data.versions);
+    }else{
+        return [];
+    }
+
+}
+
+function getSemverVersions(baseVersion,versions){
+    return versions
+        .filter(version=> semver.satisfies(version,`^${baseVersion}`))
+        .sort((a,b)=> semver.gt(b,a));
+}
+
+async function getNpmSemverVersion(baseVersion,npmName,registry){
+    const versions = await getNpmVersions(npmName,registry);
+    const newVersions = getSemverVersions(baseVersion,versions);
+    if(newVersions && newVersions.length>0){
+        return newVersions[0];
+    }
+    return null;
 }
 
 module.exports = {
     getNpmInfo,
+    getNpmVersions,
+    getNpmSemverVersion
 };
