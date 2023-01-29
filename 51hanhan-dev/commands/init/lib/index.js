ee532a85743936a457d33b8bc0e12b69aa21fc95
hanidea <hydavid@126.com>
2023-01-29 09:45:09 +0800

    dev-server

--- 51hanhan-dev/commands/init/lib/index.js
+++ 51hanhan-dev/commands/init/lib/index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const Command = require('@51hanhan-dev/command');
 const fse = require('fs-extra');
+const semver = require('semver');
 const inquirer = require('inquirer');
 const log = require('@51hanhan-dev/log');
 const TYPE_PROJECT = 'project';
@@ -17,14 +18,24 @@ class InitCommand extends Command {
     async exec(){
         try{
             // 1.准备阶段
-            const ret = await this.prepare();
-            if (ret) {
+            const projectInfo = await this.prepare();
+            if (projectInfo) {
             // 2.下载模版
+            log.verbose('projectInfo',projectInfo)
+            this.downloadTemplate();
             // 3.安装模版
             }
         }catch(e){
             log.error(e.message);
         } 
+    }
+
+    downloadTemplate(){
+        //1.通过项目模版API获取项目模版信息
+        //1.1 通过egg.js搭建一套后端系统
+        //1.2 通过npm存储项目模版
+        //1.3 将项目模版信息存储到mongodb数据库中
+        //1.4 通过egg.js获取mongodb中的数据并且通过API返回
     }
         async prepare(){
         const localPath = process.cwd();
@@ -62,7 +73,7 @@ class InitCommand extends Command {
         }
     }
         async getProjectInfo() {
-            const projectInfo = {};
+            let projectInfo = {};
             // 1.选择创建项目或组件
             const {type} = await inquirer.prompt({
                 type:'list',
@@ -80,13 +91,23 @@ class InitCommand extends Command {
             log.verbose('type',type);
             if (type === TYPE_PROJECT){
             // 2.获取项目的基本信息
-            const o = await inquirer.prompt([{
+            const project = await inquirer.prompt([{
                 type:'input',
                 name:'projectName',
                 message:'请输入项目名称',
                 default:'',
                 validate: function(v){
-                    return typeof v === 'string';
+                    const done = this.async();
+                    setTimeout(function() {
+                    // 1.首字符必须为英文字符
+                    // 2.尾字符必须为英文或数字，不能为字符
+                    // 3.字符仅允许"-_"
+                    if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+                        done(`请输入合法的项目名称`);
+                        return;
+                    }
+                    done(null, true);
+                    }, 0);
                 },
                 filter:function(v){
                     return v;
@@ -95,15 +116,34 @@ class InitCommand extends Command {
                 type:'input',
                 name:'projectVersion',
                 message:'请输入项目版本号',
-                default:'',
+                default:'1.0.0',
                 validate: function(v){
-                    return typeof v === 'string';
+                    //1.输入的首字符和尾字符必须为英文字符
+                    //2.尾字符必须为英文或数字，不能为字符
+                    //3.字符仅允许“-_”
+                    //return /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v);
+                    
+                    setTimeout(function() {
+                        if (!(!!semver.valid(v))) {
+                            done(`请输入合法的版本号`);
+                            return;
+                        }
+                        done(null, true);
+                        }, 0);
                 },
                 filter:function(v){
-                    return v;
+                    if(!!semver.valid(v)){
+                        return semver.valid(v);
+                    }else{
+                        return v;
+                    }
+                    
                 }
             }]);
-            console.log(o);
+            projectInfo = {
+                type,
+                ...project,
+            }
             }else if (type === TYPE_COMPONENT){
 
             }
