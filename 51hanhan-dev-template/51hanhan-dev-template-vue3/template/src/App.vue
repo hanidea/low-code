291b7c7f22f3b94a5ed94c45bdefe065910af15d
hanidea <hydavid@126.com>
2023-01-29 15:29:24 +0800

    no message

---
+++ 51hanhan-dev-template/51hanhan-dev-template-vue3/template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="assets/logo.png">
+  vue3 template
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  width:100%;
+  height: 100%;
+}
+</style>
