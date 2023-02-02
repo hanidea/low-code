0e99ac2c59d9e33a7084d7f18dcf17d3e60c8a15
hanidea <hydavid@126.com>
2023-02-02 13:09:22 +0800

    webpack prod

--- vue-fullstack/koa-combine/config/webpack.config.base.js
+++ vue-fullstack/koa-combine/config/webpack.config.base.js
@@ -1,18 +1,18 @@
 const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const webpackconfig = {
     target: 'node',
-    mode: 'development',
     entry: {
-        server: path.join(__dirname, 'src/index.js')
+        server: path.join(utils.APP_PATH, 'index.js')
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.join(__dirname, './dist')
+        path: utils.DIST_PATH
     },
-    devtool: 'eval-source-map',
     module: {
         rules: [
             {
@@ -26,7 +26,13 @@ const webpackconfig = {
     },
     externals: [nodeExternals()],
     plugins: [
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env':{
+                NODE_ENV: (process.env.NODE_ENV === 'production' ||
+                    process.env.NODE_ENV === 'prod') ? "'production'" : "'development'"
+            }
+        })
     ],
     node:{
         console: true,
