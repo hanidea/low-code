e53b0ad523406409238c664edbda0cadac1a3670
hanidea <hydavid@126.com>
2023-03-07 17:25:22 +0800

    editor bug

--- layui-vue/front/src/components/contents/Top.vue
+++ layui-vue/front/src/components/contents/Top.vue
@@ -21,34 +21,7 @@ export default {
     return {
       page: 0,
       limit: 20,
-      lists: [
-        {
-          uid: {
-            name: 'imooc',
-            isVip: 1
-          },
-          title: '大前端置顶',
-          content: '',
-          created: '2023-10-01 01:00:00',
-          catalog: 'ask',
-          fav: 40,
-          isEnd: 0,
-          reads: 10,
-          answer: 0,
-          status: 0,
-          isTop: 1,
-          tags: [
-            {
-              name: '精华',
-              class: 'layui-bg-red'
-            },
-            {
-              name: '热门',
-              class: 'layui-bg-blue'
-            }
-          ]
-        }
-      ]
+      lists: []
     }
   },
   components: {
