8def4b02afaa4e43c5412806f2d98e9696322528
hanidea <hydavid@126.com>
2023-11-03 18:08:27 +0800

    no message

--- fi/demo.py
+++ fi/demo.py
@@ -6,14 +6,26 @@ from flask import Flask
 # 1.以后出现bug，它可以帮助我们快速定位
 # 2.对于寻找模板文件，有一个相对路径
 app = Flask(__name__)
+app.config.from_object('config')
+print(app.config['DEBUG'])
 
 # 创建一个路由和视图函数的映射
 # 创建的是根路由
 # 访问根路由，就会执行hello_world这个函数
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/book/search/<q>/<page>')
+def search(q, page):  # put application's code here
+    """
+        q :普通关键字 isbn
+        page
+    """
+    isbn_or_key = 'key'
+    if len(q) == 13 and q.isdigit():
+        isbn_or_key = 'isbn'
+    short_q = q.replace('-','')
+    if '-' in q and len(short_q) == 10 and short_q.isdigit:
+        isbn_or_key = 'isbn'
+    pass
 
 # 运行代码
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',debug=app.config['DEBUG'],port=5001)
